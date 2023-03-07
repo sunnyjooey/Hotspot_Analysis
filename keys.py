23f3ef42531d552cdc62ac9167745650643c74f9
CRD-Dev <96207995+CRD-Dev@users.noreply.github.com>
2023-03-07 14:22:36 -0500

    Delete keys.py

--- keys.py
+++
@@ -1,7 +0,0 @@
-keys = {
-  "database_host": "undp-cb-sqlmi-crd-dev.f1de0fd669a7.database.windows.net", 
-  "database_port": "1433", 
-  "database_name": "UNDP_DW_CRD", 
-  "user": "sqlMLAdmin", 
-  "password": '=\>a={}+&BG[,y_bf,x)'
-}
